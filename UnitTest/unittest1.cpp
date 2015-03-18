7cfee76da1c58a922a484f080245da82059518c4
eugenol <eug@live.co.za>
2015-03-18 23:03:30 +0200

    add test outlines

--- UnitTest/unittest1.cpp
+++ UnitTest/unittest1.cpp
@@ -5,14 +5,36 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(StackTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(CreatStackTest)
 		{
-			// TODO: Your test code here
+			// TODO: Test stack creation
 		}
+		
+		TEST_METHOD(PushTest)
+		{
+			// TODO: Test push function
+		}
+
+		TEST_METHOD(PopTest)
+		{
+			// TODO: Test pop function
+		}
+
+		TEST_METHOD(PopEmptyTest)
+		{
+			// TODO: Test popping an empty stack
+		}
+
+		TEST_METHOD(PushFullTest)
+		{
+			// TODO: test pushing to a full stack
+		}
+
+
 
 	};
 }
