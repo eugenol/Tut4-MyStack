c21e9d365990e0c5afa0acedbbd18d3854412173
eugenol <eug@live.co.za>
2015-03-22 22:24:32 +0200

    Start Towers of Hanoi using Stack

---
+++ Hanoi/Hanoi.cpp
@@ -0,0 +1,18 @@
+// Hanoi.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../Stack/Stack.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	CStack a, b, c;
+
+	for (int i = 5; i > 0; i--)
+		a.Push(i);
+
+
+	return 0;
+}
+
