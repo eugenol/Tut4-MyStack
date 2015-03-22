da78a4d24208aff7ec08acd66cadd9c78dfd27b5
eugenol <eug@live.co.za>
2015-03-22 17:19:20 +0200

    Expand tests

--- UnitTest/unittest1.cpp
+++ UnitTest/unittest1.cpp
@@ -11,27 +11,52 @@ namespace UnitTest
 
 		TEST_METHOD(CreatStackTest)
 		{
-			// TODO: Test stack creation
+			Assert::AreEqual(1,1)
 		}
 		
 		TEST_METHOD(PushTest)
 		{
-			// TODO: Test push function
+			Stack TestStack(10);
+			int pushval = 10;
+			bool result;
+
+			result = TestStack.Push(pushval);
+			Assert::AreEqual(result,false)
 		}
 
 		TEST_METHOD(PopTest)
 		{
-			// TODO: Test pop function
+			Stack TestStack(10);
+			int pushval = 10;
+			int popval;
+			bool result;
+
+			result = TestStack.Push(pushval);
+			result = TestStack.Pop(pushval);
+			Assert::AreEqual(result, false);
+			Assert::AreEqual(popval, pushval);
 		}
 
 		TEST_METHOD(PopEmptyTest)
 		{
-			// TODO: Test popping an empty stack
+			Stack TestStack(10);
+			int popval;
+			bool result;
+
+			result = TestStack.Pop(popval);
+			Assert::AreEqual(result, true);
 		}
 
 		TEST_METHOD(PushFullTest)
 		{
-			// TODO: test pushing to a full stack
+			Stack TestStack(10);
+			bool result;
+
+			for (int i = 0; i < 12; i++)
+			{
+				result = TestStack.Push(i);
+				Assert::AreEqual(result, true);
+			}
 		}
 
 
